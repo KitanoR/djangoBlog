f442d7bea2d2d8bfb5d3d6128b5617534fc8ba08
Cayetano Rosales <cayeroza@gmail.com>
2017-09-19 11:50:57 -0600

    editar

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$',views.listar_pub), #se redirecciona a la url del blog
     url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub, name = 'postea'), #se redirecciona a la url del blog
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
